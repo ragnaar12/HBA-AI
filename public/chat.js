b9960c7d93056788167c2c2faa7a8e6d475de9cc
ragnaar12 <houssembenahmed11@gmail.com>
2025-05-21 20:04:01 +0100

    chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,7 @@
 // api/chat.js
 import fetch from "node-fetch";
 
+
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Méthode non autorisée" });
